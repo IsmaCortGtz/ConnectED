564daa3163fbd954b70d3ec1280fe1a2edbbb331
IsmaCortGtz <ismacortgtz@gmail.com>
2026-02-15 19:05:10 -0600

    Hide join call button

--- resources/js/views/Home/index.tsx
+++ resources/js/views/Home/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
+/* import React from 'react';
+import { useNavigate } from 'react-router'; */
 
 export default function HomePage() {
-  const navigate = useNavigate();
+  /* const navigate = useNavigate();
 
   const handleJoinVideoCall = () => {
     navigate('/video-call/ebe24a8d-6234-4f8b-92dc-c8e930b4f4de');
-  };
+  }; */
 
   return (
     <section style={{ padding: '40px', maxWidth: '1200px', margin: '0 auto' }}>
       <h2>Inicio</h2>
       <p>Bienvenido a ConnectED</p>
       
-      <button 
+      {/* <button 
         onClick={handleJoinVideoCall}
         style={{
           marginTop: '20px',
@@ -28,7 +28,7 @@ export default function HomePage() {
         }}
       >
         Unirse a Videollamada
-      </button>
+      </button> */}
     </section>
   );
 }
